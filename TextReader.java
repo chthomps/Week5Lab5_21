dc435a8374bbe2cfe20bf125ddfa340f24439932
chthomps <67965487+chthomps@users.noreply.github.com>
2021-08-05 19:53:16 -0500

    Update TextReader.java

--- TextReader.java
+++ TextReader.java
@@ -19,18 +19,18 @@ public class TextReader { //Week5 LabExercise1,b
 				String[]value = line.split(splitBy);  //comma separator between values
 				
 				
-				//transfer csv data to an array named "csv"
+				//transfer csv file data to an array named "csv"
 				int[][] csv = new int[3][5];
 				for (int row =0; row <=csv.length; row++)
 					for (int col =0; col <csv[row].length; col++)
-						csv[row][col] = Integer.parseInt(value);  //not seeing how to insert/convert value into a row/col location in array
+						csv[row][col] = Integer.parseInt(value);  //not seeing how to grab the appropriate string in text file and assign to desired row/col location in array
 				
 				
-				//evaluate largest value in a given row
+				//evaluate largest value in a given row (incomplete)
 				int max = Integer.parseInt(value[0]);
 				
-				for (int i=0; Integer.parseInt(value[i]) <= 5; i++) {
-					if (Integer.parseInt(value[0]) < Integer.parseInt(value [i+1])) 
+				for (int i=0; Integer.parseInt(value[i])<= 5; i++) {
+					if (Integer.parseInt(value[i]) < Integer.parseInt(value [i+1])) 
 					max = Integer.parseInt(value[i]);
 					}
 				System.out.println("Row 1: " +max);
